c62c1d151e7a6c8680e95a39d6f618a4c97d258e
Godfred <goddysmart2468@gmail.com>
2024-04-05 16:44:56 +0000

    Fetch Dashboard data

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -34,6 +34,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // fetching the last 5 invoices, sorted by data
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
