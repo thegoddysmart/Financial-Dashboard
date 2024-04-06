e03fa71005c3277fc7f4777a0706031d31201f9d
Godfred <goddysmart2468@gmail.com>
2024-04-06 08:22:49 +0000

    Mutating Data

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,64 @@
+'use server';
+
+import { custom, date, z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.date(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100; // store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy
+    const date = new Date().toISOString().split('T')[0]; // format "YYYY-MM-DD"
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices'); // revalidate the invoices page to show the new invoice
+    redirect('/dashboard/invoices'); // redirect the user to the invoices page
+
+    console.log('Creating invoice:', { customerId, amount, status });
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+    revalidatePath('/dashboard/invoices'); // this will trigger a new server request and re-render the table.
+    
+    // redirect('/dashboard/invoices'); Since this action is being called in the /dashboard/invoices path, you don't need to call redirect. 
+}
